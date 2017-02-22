0f94e89b55f4e54a50b89718bb6eabd35affe270
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-02-22 15:20:41 -0500

    Card from material-ui set up in player component

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import { Card, CardMedia, CardTitle, CardText, CardActions } from 'material-ui/Card';
 
 export default class Player extends Component {
   render() {
     return (
-      <h1>Player</h1>
+      <Card>
+        <CardMedia
+          overlay={<CardTitle title="Russel" subtitle="Offense: 999 - Defense: 1" />}
+        >
+          <img src="profile-pic.jpg" />
+        </CardMedia>
+        <CardTitle title="Card title" subtitle="Card subtitle" />
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+        <CardActions>
+
+        </CardActions>
+      </Card>
     )
   }
 }
