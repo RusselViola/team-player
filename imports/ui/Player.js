0c9b6d3459ff7e7b067ed59ae59d03f30c2bd919
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-02-22 15:34:59 -0500

    player stat chips added

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -1,5 +1,22 @@
 import React, { Component } from 'react';
 import { Card, CardMedia, CardTitle, CardText, CardActions } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import Avatar from 'material-ui/Avatar';
+import Chip from 'material-ui/Chip';
+import { indigo200, indigo300, indigo900, blue900 } from 'material-ui/styles/colors';
+
+const styles = {
+  chip: {
+    margin: 4,
+  },
+  wrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  button: {
+    margin: 12,
+  }
+};
 
 export default class Player extends Component {
   render() {
@@ -12,10 +29,78 @@ export default class Player extends Component {
         </CardMedia>
         <CardTitle title="Card title" subtitle="Card subtitle" />
         <CardText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          <Chip
+            backgroundColor={indigo200}
+            style={styles.chip}
+            >
+            <Avatar size={32} color={indigo300} backgroundColor={blue900}>
+              10
+            </Avatar>
+            Ball manipulation
+          </Chip>
+          <Chip
+            backgroundColor={indigo200}
+            style={styles.chip}
+            >
+            <Avatar size={32} color={indigo300} backgroundColor={blue900}>
+              3
+            </Avatar>
+            Kicking abilities
+          </Chip>
+          <Chip
+            backgroundColor={indigo200}
+            style={styles.chip}
+            >
+            <Avatar size={32} color={indigo300} backgroundColor={blue900}>
+              5
+            </Avatar>
+            Passing ablilities
+          </Chip>
+          <Chip
+            backgroundColor={indigo200}
+            style={styles.chip}
+            >
+            <Avatar size={32} color={indigo300} backgroundColor={blue900}>
+              3
+            </Avatar>
+            Duel/Tackling ablilities
+          </Chip>
+          <Chip
+            backgroundColor={indigo200}
+            style={styles.chip}
+            >
+            <Avatar size={32} color={indigo300} backgroundColor={blue900}>
+              3
+            </Avatar>
+            Field speed coverage
+          </Chip>
+          <Chip
+            backgroundColor={indigo200}
+            style={styles.chip}
+            >
+            <Avatar size={32} color={indigo300} backgroundColor={blue900}>
+              2
+            </Avatar>
+            Blocking ablitites
+          </Chip>
+          <Chip
+            backgroundColor={indigo200}
+            style={styles.chip}
+            >
+            <Avatar size={32} color={indigo300} backgroundColor={blue900}>
+              1
+            </Avatar>
+            Game strategy
+          </Chip>
+          <Chip
+            backgroundColor={indigo200}
+            style={styles.chip}
+            >
+            <Avatar size={32} color={indigo300} backgroundColor={blue900}>
+              10
+            </Avatar>
+            Playmaking Risks
+          </Chip>
         </CardText>
         <CardActions>
 
