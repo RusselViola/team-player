44635b79c41b1e0868c1dc069deb3e3827de8b41
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-02-24 17:05:46 -0500

    added actions to point to edit form

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -34,9 +34,12 @@ export class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      currentPlayer: tempPlayer
+      currentPlayer: tempPlayer,
+      showEditPlayer: false
     };
     this.updateCurrentPlayer = this.updateCurrentPlayer.bind(this);
+    this.showEditForm = this.showEditForm.bind(this);
+    this.showTeamStats = this.showTeamStats.bind(this);
   }
 
   renderPlayers() {
@@ -55,6 +58,33 @@ export class App extends Component {
     });
   }
 
+  showEditForm() {
+    this.setState({
+      showEditPlayer: true,
+    });
+  }
+
+  showTeamStats() {
+    this.setState({
+      showEditPlayer: false,
+    });
+  }
+
+  showForm() {
+    if(this.state.showEditPlayer === true) {
+      return (
+        <Edit
+          currentPlayer={this.state.currentPlayer}
+          showTeamStats={this.showTeamStats}
+        />
+      )
+    } else {
+      return (
+        <TeamStats />
+      );
+    }
+  }
+
   render() {
     return (
       <MuiThemeProvider>
@@ -71,6 +101,7 @@ export class App extends Component {
             <div className="col s12 m7">
               <Player
                 player={this.state.currentPlayer}
+                showEditForm={this.showEditForm}
               />
             </div>
             <div className="col s12 m5">
@@ -81,7 +112,7 @@ export class App extends Component {
                 </List>
               <Divider />
             </div>
-            <div className="col s12 m5"><TeamStats /></div>
+            <div className="col s12 m5">{this.showForm()}</div>
           </div>
         </div>
 
